40a65dc20fbcea7b9a624d542a704335bf1d13a6
NN98 <jkllhgb@gmail.com>
2019-04-18 09:54:13 +0900

    #2 template setting

---
+++ app/src/main/java/com/example/baekjoon_ta/TemplateActivity.java
@@ -0,0 +1,13 @@
+package com.example.baekjoon_ta;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class TemplateActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template);
+    }
+}
