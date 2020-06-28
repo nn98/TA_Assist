4afc5294b09573bd0bacef2c91f9aaf44d34d605
nn98 <jkllhgb@gmail.com>
2020-06-29 00:41:34 +0900

    #1 student/class list up

--- src/Web/StatusManager.java
+++ src/Web/StatusManager.java
@@ -15,15 +15,17 @@ public class StatusManager {
 	public static void main(String[] args) {
 		try { 
 			Scanner s=new Scanner(System.in);
-			int n,i,j=1;
+			int n,i,j=1,p;
 			while(j>0) {
 				for(i=0;i<StudentInfo.ClassNumber;i++)
 					System.out.print(i+1+": "+StudentInfo.Class[i]+"  ");
 				System.out.print("\n0 to end: ");
 				n=s.nextInt()-1;
+				System.out.print("C?: 0-N 1-Y ");
+				p=s.nextInt();
 				if(n<0)break;
 				for(i=0;i<StudentInfo.StudentNumber[n];i++)
-					Desktop.getDesktop().browse(new URI("https://www.acmicpc.net/status?problem_id=&user_id="+StudentInfo.ID_LIST[n][i]+"&language_id=-1&result_id=4"));
+					Desktop.getDesktop().browse(new URI("https://www.acmicpc.net/status?problem_id=&user_id="+StudentInfo.ID_LIST[n][i]+"&language_id=-1&result_id="+(p>0?4:-1)));
 			}
 		}
 		catch (IOException e) { 
